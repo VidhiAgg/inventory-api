70951e3f23265cbe34ba94835b79588e526be4a5
Vidhi Aggarwal <vidhiaggarwal@Vidhis-MacBook-Pro.local>
2024-04-13 01:33:31 +0530

    Corrected the code

--- dbConnect/dbConnect.js
+++ dbConnect/dbConnect.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
-const mongoURI = process.env.MONGODB;
-console.log(process.env.MONGODB);
-console.log(mongoURI);
-const intializeDBConnection = async()=>{
-    try {
-        mongoose.connect(mongoURI).then(()=>{
-            console.log("Connected to MongoDb.");
-        }).catch(error => {
-            console.error("Error connecting the Mongodb", error);
-        })
-    } catch (error) {
-        console.error("Error connecting tp MongoDb: ", error);
-    }
-}
+//const mongoURI = process.env.MONGODB;
+// console.log(process.env.MONGODB);
+// console.log(mongoURI);
 
-module.exports = {intializeDBConnection}
+
+
+const intializeDBConnection = async () => {
+    const DB_USER = process.env.DB_USER;
+    const DB_PASSWORD = process.env.DB_PASSWORD;
+    const dbURL =    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@neog.1tvoo8d.mongodb.net/?retryWrites=true&w=majority`;
+    console.log(dbURL);
+    try {
+      const connect = await mongoose.connect(dbURL);
+      console.log("Database connected successfully...");
+    } catch (error) {
+      console.error(error);
+      process.exit();
+    }
+  };
+  
+  module.exports = { intializeDBConnection };
